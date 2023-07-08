a43c4623343869597dd7017e3a8abb427be0a5c5
Ricardo Vela <aricav96@gmail.com>
2023-07-07 22:01:05 -0500

    Update session-choose.sh (return of stumpwm)

--- bin/bin/session-choose.sh
+++ bin/bin/session-choose.sh
@@ -3,7 +3,7 @@
 # Script to choose a window manager
 # Simply add or remove wms to the wms array
 
-wms=( awesome openbox-session none )
+wms=( stumpwm awesome none )
 wms_size=${#wms[@]}
 
 echo 
@@ -12,28 +12,28 @@ echo
 # We use seq to be able to identify wms by their index
 for index in $(seq 0 $((wms_size - 1)))
 do
-    echo [$index] - ${wms[$index]}
+    echo ["$index"] - "${wms[$index]}"
 done
 
 read -p "Choose your wm (defaults to ${wms[0]}): " id
 
-if [[ $id > $((wms_size - 1)) ]]
+if [[ "$id" > "$((wms_size - 1))" ]]
 then
    id=0
 fi
 
-session=${wms[$id]}
+session="`command -v ${wms[$id]}`"
 
-case $session in
+case "$session" in
     none) 
         exit 1
         ;;
     *)
-        if command -v $session
+        if [[ -n "$session" ]]
         then
-            echo $session > last-session
+            echo "$session" > ~/last-session
         else
-            echo $session is not installed
+            echo "$session" is not installed
             exit 1
         fi
         ;;
